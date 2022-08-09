adb29c19e540490b3fd6f69405fbaeffdd7f8971
drc9472 <108583927+drc9472@users.noreply.github.com>
2022-08-09 11:51:32 -0400

    Set up app folder and tested API Keys

---
+++ APP/dashboard.py
@@ -0,0 +1,13 @@
+from typing import no_type_check
+import os
+from pandas import read_csv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+key1 = os.environ.get("nyc_odata_api_key")
+key2 = os.environ.get("alphavantage_api_key")
+nyc_odata_csv_filepath = f"https://data.cityofnewyork.us/resource/9ck6-2jew.json?$limit=27000&$$app_token={key1}"
+alphavantage_csv_filepath = f"https://www.alphavantage.co/query?function=INFLATION&datatype=csv&apikey={key2}"
+print(nyc_odata_csv_filepath)
+print(alphavantage_csv_filepath)

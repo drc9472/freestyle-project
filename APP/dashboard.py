2e5622a9f8d63808e9c66c8e76946395dd6ac67e
drc9472 <108583927+drc9472@users.noreply.github.com>
2022-08-09 16:39:36 -0400

    Corrected OData url and added verification

--- APP/dashboard.py
+++ APP/dashboard.py
@@ -7,7 +7,14 @@ load_dotenv()
 
 key1 = os.environ.get("nyc_odata_api_key")
 key2 = os.environ.get("alphavantage_api_key")
-nyc_odata_csv_filepath = f"https://data.cityofnewyork.us/resource/9ck6-2jew.json?$limit=27000&$$app_token={key1}"
+nyc_odata_csv_filepath = f"https://data.cityofnewyork.us/resource/9ck6-2jew.csv?$limit=27000&$$app_token={key1}"
 alphavantage_csv_filepath = f"https://www.alphavantage.co/query?function=INFLATION&datatype=csv&apikey={key2}"
 print(nyc_odata_csv_filepath)
 print(alphavantage_csv_filepath)
+
+alphavantage_df = read_csv(alphavantage_csv_filepath)
+print(alphavantage_df.columns)
+nyc_odata_df = read_csv(nyc_odata_csv_filepath)
+print(nyc_odata_df.columns)
+    
+
